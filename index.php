a4a76b1ea76aa304c2989405db082f094b9e1ffd
Augustina Wear <awear@cnm.edu>
2017-07-11 13:37:51 -0600

    All about me page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOC TYPE html>
+<html>
+	<head>
+		<title>allaboutme</title>
+	</head>
+	<body>
+		<p>I am a grandmother of nine ages ranging from thirteen to eighteen months.
+			I am also an artist my prefered medium is printmaking specializing in intaglio and lithographs.
+			Learning the skills Php,MySQL & JavaScript will help me in my business and expand my skill set. </p><p>
+			I  am origniallly from New England and it has taken me a few years to be acclimated to New Mexico weather.
+			I enjoy the heat now and love green chili on my eggs.</p>
+		<img src="images/augustinawear.jpg">
+	</body>
+</html>
