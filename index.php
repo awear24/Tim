3dfa43d19bb3dad145f3856df293128faa0b69a5
steven hebert <shebert2@cnm.edu>
2017-07-11 16:32:30 -0600

    test

--- index.php
+++ index.php
@@ -3,23 +3,28 @@
 	<head>
 		<title>MadLibAboutMe</title>
 	</head>
+
+
 	<style>
 		body {
-
 			background-color: rgb(<0>,<100>,<200>)
 			background-image: url("http://cowsay.morecode.org/static/cowsay.png");
 			background-repeat;
 		}
 	</style>
+
+
 	<body>
+
 		<h1>welcome to Augustina's MadLibography</h1>
-		<p>
-			Augustina Wear is a heavy metal author who has written about everything from Deep Dive Coding to Pickled Pants
-			and has won a nobel laureate for her nonfiction book, "The Programmer's Guide to copypasta.” She lives in
-			Antartica with her family and 3.1415926535897932384626433832795028841971693 enchanted dragons. She is a
-			library science major at the MVD and has enjoyed printmaking in deep space, chugging iowaska tea, and
-			playing tug of war with her neighbor, Tonald Drump. She aspires to someday be the first full-time author
+
+		<p>Augustina Wear is a heavy metal author who has written about everything from Deep Dive Coding to Pickled Pants
+			and has won a nobel laureate for her nonfiction book, "The Programmer's Guide to copypasta.” She lives in <br>
+			Antartica with her family and 3.1415926535897932384626433832795028841971693 enchanted dragons. She is a <br>
+			library science major at the MVD and has enjoyed printmaking in deep space, chugging iowaska tea, and <br>
+			playing tug of war with her neighbor, Tonald Drump. She aspires to someday be the first full-time author <br>
 			to breath on the moon!</p>
+
 		<img src="images/augustinawear.jpg" >
 	</body>
 </html>
